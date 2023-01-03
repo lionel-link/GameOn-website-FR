d5f291556a6ddce512b3fb8e929a225fcacce3d8
lionel-link <53856679+lionel-link@users.noreply.github.com>
2023-01-03 10:53:10 +0100

    modal bugs fixed

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,7 +10,10 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const closeButton = document.querySelector(".close");
+const form = document.getElementById("form")
+const formData = document.getElementById("formData")
+const MessageSuccess = document.getElementById("message-success")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +23,82 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close modal event
+closeButton.addEventListener("click", closeModal)
 
+
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// fonction submit
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  handleData(e);
+  closeModal();
+  MessageSuccess.style.display="block"
+  setInterval(()=>{MessageSuccess.style.display="none"}, 4000)
+
+})
+
+function handleData(e) {
+  var form_data = new FormData(document.getElementById("form"));
+  if (!form_data.has("location")) {
+    document.getElementById("chk_option_location_error").style.display = "block";
+  }else {
+    document.getElementById("chk_option_location_error").style.display = "none";
+  }
+
+  if (!form_data.has("termofuse")) {
+    document.getElementById("chk_option_termofuse_error").style.display = "block";
+  }else {
+    document.getElementById("chk_option_termofuse_error").style.display = "none";
+  }
+
+  console.log(form_data.get("birthdate"))
+
+  if (form_data.get("first").length <= 1) {
+    document.getElementById("chk_option_first_error").style.display = "block";
+  }
+  else {
+    document.getElementById("chk_option_first_error").style.display = "none";
+  }
+
+  if (form_data.get("last").length <= 1) {
+    document.getElementById("chk_option_last_error").style.display = "block";
+  }
+  else {
+    document.getElementById("chk_option_last_error").style.display = "none";
+  }
+
+  if (!validateEmail(form_data.get("email"))) {
+    document.getElementById("chk_option_email_error").style.display = "block";
+  }
+  else {
+    document.getElementById("chk_option_email_error").style.display = "none";
+  }
+
+  if (!form_data.get("birthdate")) {
+    document.getElementById("chk_option_birthdate_error").style.display = "block";
+  }
+  else {
+    document.getElementById("chk_option_birthdate_error").style.display = "none";
+  }
+  if (!form_data.get("quantity")) {
+    document.getElementById("chk_option_quantity_error").style.display = "block";
+  }
+  else {
+    document.getElementById("chk_option_quantity_error").style.display = "none";
+  }
+
+  return false;
+}
+
+
+
+
+function validateEmail(email) {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
