23ec913eaf2beb9cc8da05cabcae788b2449590b
Nicolas Challeil <nicolas.challeil@openclassrooms.com>
2023-01-04 09:23:29 +0100

    wip

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -27,27 +27,43 @@ function launchModal() {
 closeButton.addEventListener("click", closeModal)
 
 
-// Close modal form
 function closeModal() {
   modalbg.style.display = "none";
 }
 
 // fonction submit
 form.addEventListener('submit', (e) => {
+    // const form = e.target; // meilleur façon de faire, recuperer le form le plus tot, et le passer a validateData, plutot que de passer e
+
   e.preventDefault();
-  handleData(e);
+  if (validateData(form)) {
+
+  } else {
+    
+  }
   closeModal();
   MessageSuccess.style.display="block"
   setInterval(()=>{MessageSuccess.style.display="none"}, 4000)
 
 })
 
-function handleData(e) {
-  var form_data = new FormData(document.getElementById("form"));
-  if (!form_data.has("location")) {
-    document.getElementById("chk_option_location_error").style.display = "block";
-  }else {
+function toggleInputError(message, elementId, visibility) {
+  const element = document.getElementById(elementId);
+  element.innerHTML = "<p class='text-label'>" + message + "</p>";
+  element.style.display = visibility ? "none" : "block";
+}
+
+function validateData(form) {
+  var form_data = new FormData(form);
+
+  const hasLocation = form_data.has("location");
+
+  // toggleInputError("Veuillez selectionner votre ville", 'chk_option_location_error', form_data.has("location"));
+
+  if (form_data.has("location")) {
     document.getElementById("chk_option_location_error").style.display = "none";
+  }else {
+    document.getElementById("chk_option_location_error").style.display = "block";
   }
 
   if (!form_data.has("termofuse")) {
@@ -57,6 +73,12 @@ function handleData(e) {
   }
 
   console.log(form_data.get("birthdate"))
+
+  // const firstNameTooShort = form_data.get("first").length <= 1;
+  // const firstNameTooLong = form_data.get("first").length > 50;
+
+  // toggleInputError("Votre nom est trop court, il doit faire plus de 2 characteres", 'chk_option_first_error', firstNameTooShort);
+  // toggleInputError("Votre nom est trop long, il doit pas faire plus de 50 characteres", 'chk_option_first_error', firstNameTooLong); // example, non demandée dans le projet
 
   if (form_data.get("first").length <= 1) {
     document.getElementById("chk_option_first_error").style.display = "block";
@@ -92,7 +114,11 @@ function handleData(e) {
     document.getElementById("chk_option_quantity_error").style.display = "none";
   }
 
-  return false;
+  if (hasLocation && hasName /*&& hasFirstName...*/) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 
