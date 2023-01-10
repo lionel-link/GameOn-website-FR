78024456d208c823abc7a9af56d72528654946c4
lionel-link <53856679+lionel-link@users.noreply.github.com>
2023-01-10 11:02:31 +0100

    bug fixed

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,6 +14,14 @@ const closeButton = document.querySelector(".close");
 const form = document.getElementById("form")
 const formData = document.getElementById("formData")
 const MessageSuccess = document.getElementById("message-success")
+const firstname = document.getElementById("first")
+const lastname = document.getElementById("last")
+const email = document.getElementById("email")
+const birthdate = document.getElementById("birthdate")
+const quantity = document.getElementById("quantity")
+const tournoi = document.getElementById("location-error")
+const termOfUse = document.getElementById("termOfUse")
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -35,70 +43,87 @@ function closeModal() {
 // fonction submit
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  handleData(e);
-  closeModal();
-  MessageSuccess.style.display="block"
-  setInterval(()=>{MessageSuccess.style.display="none"}, 4000)
-
+  if (handleData(e)) {
+    closeModal()
+    MessageSuccess.style.display = "block"
+    setInterval(() => { MessageSuccess.style.display = "none" }, 4000)
+  }
 })
 
 function handleData(e) {
-  var form_data = new FormData(document.getElementById("form"));
-  if (!form_data.has("location")) {
-    document.getElementById("chk_option_location_error").style.display = "block";
-  }else {
-    document.getElementById("chk_option_location_error").style.display = "none";
-  }
+  
+  var form_data = new FormData(e.target);
+  var firstCheck = true
+  var lastCheck = true
+  var emailCheck = true
+  var birthdateCheck = true
+  var quantityCheck = true
+  var tournoiCheck = true
+  var termOfUseCheck = true
 
-  if (!form_data.has("termofuse")) {
-    document.getElementById("chk_option_termofuse_error").style.display = "block";
-  }else {
-    document.getElementById("chk_option_termofuse_error").style.display = "none";
-  }
+  if (form_data.get("first").length < 2) {
+    showError(firstname, "firstname", "Veuillez entrer plus de 2 lettres")
+    firstCheck = false
+  } else { hideError("firstname"); firstCheck = true}
 
-  console.log(form_data.get("birthdate"))
-
-  if (form_data.get("first").length <= 1) {
-    document.getElementById("chk_option_first_error").style.display = "block";
-  }
-  else {
-    document.getElementById("chk_option_first_error").style.display = "none";
-  }
-
-  if (form_data.get("last").length <= 1) {
-    document.getElementById("chk_option_last_error").style.display = "block";
-  }
-  else {
-    document.getElementById("chk_option_last_error").style.display = "none";
-  }
+  if (form_data.get("last").length < 2) {
+    showError(lastname, "lastname", "Veuillez entrer plus de 2 lettres")
+    lastCheck = false
+  } else { hideError("lastname"); lastCheck = true }
 
   if (!validateEmail(form_data.get("email"))) {
-    document.getElementById("chk_option_email_error").style.display = "block";
-  }
-  else {
-    document.getElementById("chk_option_email_error").style.display = "none";
-  }
+    showError(email, "email", "Veuillez entrer un email valide")
+    emailCheck = false
+  } else { hideError("email"); emailCheck = true }
 
   if (!form_data.get("birthdate")) {
-    document.getElementById("chk_option_birthdate_error").style.display = "block";
-  }
-  else {
-    document.getElementById("chk_option_birthdate_error").style.display = "none";
-  }
-  if (!form_data.get("quantity")) {
-    document.getElementById("chk_option_quantity_error").style.display = "block";
-  }
-  else {
-    document.getElementById("chk_option_quantity_error").style.display = "none";
-  }
+    showError(birthdate, "birthdate", "Veuillez entrer votre date de naissance")
+    birthdateCheck = false
+  } else { hideError("birthdate"); birthdateCheck = true }
 
+  if (!form_data.get("quantity")) {
+    showError(quantity, "quantity", "Veuillez entrer votre nombre de participation")
+    quantityCheck = false
+  } else { hideError("quantity"); quantityCheck = true }
+
+  if (!form_data.has("location")) {
+    showError(tournoi, "location", "Veuillez choisir une ville")
+    firstCheck = false
+  } else { hideError("location"); tournoiCheck = true }
+
+  if (!form_data.has("termofuse")) {
+    showError(termOfUse, "termOfUse", "Veuillez accepter les conditions d'utilisation.")
+    termOfUseCheck = false
+  } else { hideError("termOfUse"); termOfUseCheck = true }
+
+  if (firstCheck & lastCheck & emailCheck & birthdateCheck & quantityCheck & tournoiCheck & termOfUseCheck) {
+    return true;
+  }
+  
   return false;
 }
 
 
-
-
 function validateEmail(email) {
-  var re = /\S+@\S+\.\S+/;
+  var re = /\w+@\w+\.\w+/;
   return re.test(email);
+}
+
+
+
+function showError(element, errorBlock, errorMessage) {
+  if (document.getElementsByClassName(errorBlock).length == 0) {
+    let newDiv = document.createElement("div");
+    newDiv.className = errorBlock;
+    newDiv.innerHTML += '<p class="text-label">' + errorMessage + '</p>'
+    element.after(newDiv)
+  }
+}
+
+function hideError(errorBlock) {
+  error = []
+  error = document.getElementsByClassName(errorBlock)
+  if (error.length >= 1) {
+    error[0].remove()
+  }
 }
